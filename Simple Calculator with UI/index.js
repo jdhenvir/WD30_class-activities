10c32eccc74ceffef063ad99e4302b9af1503a57
James Dhenvir <118252954+jdhenvir@users.noreply.github.com>
2023-01-11 11:35:10 +0800

    update

--- Simple Calculator with UI/index.js
+++ Simple Calculator with UI/index.js
@@ -50,17 +50,75 @@ const clear_input = () => {
 
 const calculate = ()=> {
     let val = display.innerText;
+    let val_arr;
     if(val.endsWith("+")||val.endsWith("-")||val.endsWith("*")||val.endsWith("/")){
         //will remove first the trailing operator and consider it as not part of the equation.
         delete_input();
         // update the val...
         val = display.innerText;
     }
-    // javascript eval function. magically do the operation correctly! dont worry about operator precedence anymore!
-    display.innerText = eval(val); 
+    val_arr = val.split(' ');
+    console.log(val_arr);
+    // first loop run to evaluate multiplications or division operations.
+    for (let i = 0; i < val_arr.length; i++) {
+        if (val_arr[i] == '*'){
+            val_arr[i] = val_arr[i + 1] * val_arr[i - 1];
+            val_arr.splice(i + 1, 1)
+            val_arr.splice(i - 1, 1)
+            i--;
+            console.log(val_arr);
+            continue;
+        }
+        if (val_arr[i] === '/') {
+            val_arr[i] = val_arr[i - 1] / val_arr[i + 1];
+            val_arr.splice(i + 1, 1)
+            val_arr.splice(i - 1, 1);
+            i--;
+            console.log(val_arr);
+        }
+        }
+
+    // second loop run to evaluate addition or subtraction operations.
+    for (let i = 0; i < val_arr.length; i++) {
+        if (val_arr[i] == '+'){
+            val_arr[i] = (+val_arr[i - 1]) + (+val_arr[i + 1]);
+            val_arr.splice(i + 1, 1)
+            val_arr.splice(i - 1, 1)
+            i--;
+            console.log(val_arr);
+            continue;
+        }
+        if (val_arr[i] === '-') {
+            val_arr[i] = (+val_arr[i - 1]) - (+val_arr[i + 1]);
+            val_arr.splice(i + 1, 1)
+            val_arr.splice(i - 1, 1);
+            i--;
+            console.log(val_arr);
+        }
+    }
+
+    // Checking to see if the result have a decimal point if so round it up to two decimal points
+    if (val_arr[0] % 1 !== 0) {
+        val_arr[0] = val_arr[0].toFixed(2);
+    }
+
+    // print the result in display
+    display.innerText = val_arr[0]; 
     expresion_display.innerText = val + " = ";
     new_entry = true;
 }
+
+function operate(operation) {
+   
+    /* Checking to see if the result have a decimal point if so round it up to two decimal points for better readability else just set operation result to it's original value*/
+    if (operation[0] % 1 !== 0) {
+      operationResult = Math.round(operation[0] * 100) / 100;
+    } else {
+      operationResult = operation[0];
+    }
+    return operationResult;
+  }
+
 
 document.querySelector(".num-btn-grp").addEventListener('click', num_input );
 document.querySelector(".oparators-btn-grp").addEventListener('click', operator_input );
